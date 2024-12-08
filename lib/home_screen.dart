d9fdec0b88e899b0771ca609b67297eb4a518487
Md Mizanul Islam <islammizanul.7@gmail.com>
2024-12-08 20:11:48 +0600

    google_map_with_geolocator

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,7 +7,6 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
 class _HomeScreenState extends State<HomeScreen> {
   late GoogleMapController googleMapController;
 
